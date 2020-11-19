0e9a192da12942d434b3205e823cf6820f1ccad7
Gynanasekar <gynanasekarsel14@gmail.com>
2020-11-19 20:26:23 +0530

    My First Project Code

---
+++ src/org/FirstJavaProject/FirstJavaProject.java
@@ -0,0 +1,9 @@
+package org.FirstJavaProject;
+
+public class FirstJavaProject {
+
+	public static void main(String[] args) {
+		System.out.println("Hi G..!! Welcome to your first project");
+	}
+
+}
